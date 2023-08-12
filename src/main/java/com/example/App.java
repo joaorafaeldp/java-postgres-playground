9320f84b3a72c2411be003b8478c47734bb2d192
Joao Rafael Duarte Pereira <joaorafaeldp@gmail.com>
2023-08-12 23:38:35 +0000

    Adicionado teste

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -2,7 +2,7 @@ package com.example;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.SQLException
 
 public class App {
     private static final String PASSWORD = "";
